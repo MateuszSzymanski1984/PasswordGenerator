c3837ee85c58f42474a1d3a793b7461d44945c1b
Mateusz Szymański <mateusz.szymanski.dev@gmail.com>
2023-09-10 18:41:14 +0200

    New test

--- src/test/java/org/example/PasswordGeneratorAppTest.java
+++ src/test/java/org/example/PasswordGeneratorAppTest.java
@@ -2,6 +2,9 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PasswordGeneratorAppTest {
@@ -24,10 +27,57 @@ class PasswordGeneratorAppTest {
         assertTrue(password.matches("[A-Z]+"), "Contains only uppercase letters");
     }
     @Test
+    void testGeneratePasswordWithDigits() {
+        PasswordGeneratorApp generator = new PasswordGeneratorApp();
+        String password = generator.generatePassword(8, false, false, true, false);
+        assertNotNull(password);
+        assertEquals(8, password.length());
+        assertTrue(password.matches("[0-9]+"), "Contains only digits");
+    }
+
+    @Test
+    void testGeneratePasswordWithSpecialCharacters() {
+        PasswordGeneratorApp generator = new PasswordGeneratorApp();
+        String password = generator.generatePassword(15, false, false, false, true);
+        assertNotNull(password);
+        assertEquals(15, password.length());
+        assertTrue(password.matches("[!@#$%^&*()_+\\-\\[\\]{}|;:<>,.?]+"), "Contains only special characters");
+    }
+    @Test
+    void testGeneratePasswordLength() {
+        PasswordGeneratorApp generator = new PasswordGeneratorApp();
+        String password1 = generator.generatePassword(8, true, false, false, false);
+        assertEquals(8, password1.length());
+
+        String password2 = generator.generatePassword(12, false, true, true, false);
+        assertEquals(12, password2.length());
+
+        String password3 = generator.generatePassword(20, true, true, true, true);
+        assertEquals(20, password3.length());
+    }
+    @Test
     void testGeneratePasswordWithEmptyLength() {
         PasswordGeneratorApp generator = new PasswordGeneratorApp();
         String password = generator.generatePassword(0, true, false, false, false);
         assertEquals("", password); // Oczekujemy pustego hasła
     }
+    @Test
+    void testGeneratePasswordWithAllOptionsDisabled() {
+        PasswordGeneratorApp generator = new PasswordGeneratorApp();
+        String password = generator.generatePassword(10, false, false, false, false);
+        assertEquals("", password); // Oczekujemy pustego hasła
+    }
+    @Test
+    void testGenerateRandomness() {
+        PasswordGeneratorApp generator = new PasswordGeneratorApp();
+        int numberOfPasswords = 100;
+        Set<String> passwords = new HashSet<>();
 
+        for (int i = 0; i < numberOfPasswords; i++) {
+            String password = generator.generatePassword(10, true, true, true, true);
+            passwords.add(password);
+        }
+
+        assertEquals(numberOfPasswords, passwords.size()); // Upewniamy się, że wszystkie hasła są różne
+    }
 }
